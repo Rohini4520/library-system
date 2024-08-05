3a052e3d815a8ed29a2ce8afd076887bf8a50f31
Rohini Patil <rohine452001@gmail.com>
2024-08-05 15:44:40 +0530

    Adding transactions

---
+++ src/main/java/com/acciojob/library_system/Models/Book.java
@@ -0,0 +1,29 @@
+package com.acciojob.library_system.Models;
+
+import com.acciojob.library_system.Enum.Genre;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    private Integer bookId;
+
+    private String bookName;
+
+    private Integer noOfPages;
+
+    private String authorName;
+
+@Enumerated(value = EnumType.STRING)
+    private Genre genre;
+
+}
