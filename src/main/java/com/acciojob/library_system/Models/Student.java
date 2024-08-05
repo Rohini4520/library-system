0990bbb1f069f59186f040976ddebc97d459a845
Rohini Patil <rohine452001@gmail.com>
2024-08-05 11:57:05 +0530

    code for library system

---
+++ src/main/java/com/acciojob/library_system/Models/Student.java
@@ -0,0 +1,33 @@
+package com.acciojob.library_system.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer StudentId;
+
+    private String name;
+
+    private Integer age;
+
+    private String branch;
+
+    @Column(unique = true,length = 300)
+    private String emailId;
+
+    private String address;
+}
+
+
