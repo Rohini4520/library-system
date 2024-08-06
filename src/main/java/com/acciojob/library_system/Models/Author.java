bbab63df1e683f4d631f82a94a0e18c658d5bc2a
Rohini Patil <rohine452001@gmail.com>
2024-08-06 14:32:27 +0530

    Adding author

---
+++ src/main/java/com/acciojob/library_system/Models/Author.java
@@ -0,0 +1,19 @@
+package com.acciojob.library_system.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Entity
+//@Table
+@Getter
+@Setter
+
+
+public class Author {
+
+
+}
